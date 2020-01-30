ddecd8a2bcb9f5e3d99fc2b34cbf4765d48e15d0
Berta Matu <info.berta@gmail.com>
2020-01-30 20:26:46 +0200

    complete

--- src/main/java/lt/berta/spring3App/Repositories/ProductRepository.java
+++ src/main/java/lt/berta/spring3App/Repositories/ProductRepository.java
@@ -4,6 +4,6 @@ import lt.berta.spring3App.Entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-
+    // JpaRepository interface extends CrudRepository and provides finder methods out of the box;
 }
 
